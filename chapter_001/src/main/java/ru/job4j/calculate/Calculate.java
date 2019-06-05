b6e20fa8b44d98ac03d2078b431120c80e2390c9
Sergei Baranov <avtomatika@vens.local>
2019-06-05 18:12:48 +0300

    1.3. JavaDoc. Code convention[#146776]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,21 @@
 package ru.job4j.calculate;
+/**
+
+* Calculate.
+
+*
+
+*@author Sergei Baranov (mariosb84@mail.ru)
 
 public class Calculate {
+
+/**
+
+*Main.
+
+*@param args - args.
+
+*/
 
 
 public static void main (String[] args) {
