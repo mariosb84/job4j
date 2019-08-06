7f89ba83dd53f29801a31d2ff348f24091af3066
Sergei Baranov <mariosb84@mail.ru>
2019-08-06 21:53:55 +0300

    4. Расстояние между точками в трехмерном пространстве.[#146850]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -23,6 +23,10 @@ public class Point {
      */
     private int y;
     /**
+     * And this is the field of the object. It is available only to a specific object.
+     */
+    private int z;
+    /**
      * A constructor that takes the initial state of a point object
      *       * @param first x coordinate
      *       * @param second y coordinate
@@ -31,10 +35,27 @@ public class Point {
         this.x = first;
         this.y = second;
     }
+    /**
+     * A constructor that takes the initial state of a point object
+     *       * @param first x coordinate
+     *       * @param second y coordinate
+     *       * @param third z coordinate
+     */
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+    public void info3d() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -25,12 +25,34 @@ public class PointTest {
         assertThat(result, is(10D));
     }
     /**
+     * Test distance3d.
+     */
+    @Test
+    public void whenZeroAndTwentyThenTwenty() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(0, 0, 20);
+        double result = first.distance3d(second);
+        first.info3d();
+        second.info3d();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(20D));
+    }
+    /**
      * Test checkItself.
      */
     @Test
     public void whenCheckItself() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+    /**
+     * Test checkItself3d.
+     */
+    @Test
+    public void whenCheckItself3d() {
+        Point point = new Point(0, 0,0);
+        double result = point.distance3d(point);
         assertThat(result, is(0D));
     }
     /**
@@ -42,5 +64,15 @@ public class PointTest {
         first.info();
         Point second = new Point(2, 2);
         second.info();
+    }
+    /**
+     * Test showInfo3d.
+     */
+    @Test
+    public void whenShowInfo3d() {
+        Point first = new Point(1, 1,1);
+        first.info3d();
+        Point second = new Point(2, 2,2);
+        second.info3d();
     }
 }
