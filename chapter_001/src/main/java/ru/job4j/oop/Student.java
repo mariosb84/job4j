17be0276f8cd8c142fd678fc96d6f32bfee62a8c
Sergei Baranov <mariosb84@mail.ru>
2020-07-27 12:11:27 +0300

    1.2. Вызов метода объекта [#328830]

--- chapter_001/src/main/java/ru/job4j/oop/Student.java
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -1,4 +1,25 @@
 package ru.job4j.oop;
 
 public class Student {
+    public void music() {
+        System.out.println("tra tra tra");
+    }
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+    public void music3() {
+        music();
+        music();
+        music();
+    }
+    public void song3() {
+        song();
+        song();
+        song();
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music3();
+        petya.song3();
+    }
 }
