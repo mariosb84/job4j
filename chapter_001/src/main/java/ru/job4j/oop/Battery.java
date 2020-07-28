086d2b85e45cd32288b2aec28be865d3d13e20ac
Sergei Baranov <mariosb84@mail.ru>
2020-07-28 14:11:53 +0300

    1.7. Состояние объекта [#328834]

--- chapter_001/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -6,18 +6,15 @@ public class Battery {
         this.load = size;
     }
     public void exchange(Battery another) {
-for (int i = 0; this.load > 0; i--) {
-    this.load--;
-    another.load++;
-    System.out.println(this.load);
-}
+            another.load += this.load;
+            this.load -= this.load;
+            System.out.println(this.load);
     }
     public static void main(String[] args) {
         Battery batteryOne = new Battery(100);
         Battery batterySecond = new Battery(0);
         System.out.println("Размер первой батареи : " + batteryOne.load);
         System.out.println("Размер второй батареи : " + batterySecond.load);
-        System.out.println();
         batteryOne.exchange(batterySecond);
         System.out.println("Размер первой батареи : " + batteryOne.load);
         System.out.println("Размер второй батареи : " + batterySecond.load);
