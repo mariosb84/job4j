2ca1cfd019ee82ed5b7ede7c9abfc132ee67044b
Sergei Baranov <mariosb84@mail.ru>
2020-08-18 17:42:14 +0300

    2. Scanner и чтение числа из консоли. [#328794]

--- chapter_001/src/main/java/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/io/Matches.java
@@ -4,33 +4,64 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
+        Matches matches = new Matches();
         Scanner input = new Scanner(System.in);
         boolean run = true;
         int amount = 11;
+        int countOne = 0;
+        int countTwo = 0;
         while (run) {
-                System.out.println("Берите спичку(от 1 до 3)");
-                System.out.println("Игрок 1: ");
-                int select = Integer.valueOf(input.nextLine());
+            System.out.println("Берите спичку(от 1 до 3)");
+            System.out.println("Игрок 1: ");
+            int select = Integer.valueOf(input.nextLine());
+            Boolean validationOne = select <= 3 && select >= 1;
+            if (validationOne) {
                 amount -= select;
-                System.out.println("Осталось " + amount + " спичек");
-                if (amount < 1) {
-                    run = false;
-                    System.out.println("Игра окончена");
-                    System.out.println("Выиграл 1 игрок");
-                }
-                System.out.println("Берите спичку(от 1 до 3)");
-                System.out.println("Игрок 2: ");
-                int selectTwo = Integer.valueOf(input.nextLine());
+                matches.showFour(amount);
+                countOne++;
+            } else {
+                matches.showFive();
+                continue;
+            }
+            System.out.println("Игрок 2: ");
+            int selectTwo = Integer.valueOf(input.nextLine());
+            Boolean validationTwo = selectTwo <= 3 && selectTwo >= 1;
+            if (validationTwo) {
                 amount -= selectTwo;
-                System.out.println("Осталось " + amount + " спичек");
+                matches.showFour(amount);
+                countTwo++;
+            } else {
+                matches.showFive();
+                continue;
+            }
             if (amount < 1) {
                 run = false;
-                System.out.println("Игра окончена");
-                System.out.println("Выиграл 2 игрок");
+                matches.showThree();
+                if (countOne > countTwo) {
+                    matches.showOne();
+                } else {
+                    matches.showTwo();
+                }
+                }
+        }
             }
-            }
 
 
 
+    public void showOne() {
+        System.out.println("Выиграл 1 игрок");
     }
+    public void showTwo() {
+        System.out.println("Выиграл 2 игрок");
+    }
+    public void showThree() {
+        System.out.println("Игра окончена");
+    }
+    public void showFour(int amount) {
+        System.out.println("Осталось спичек : " + amount);
+    }
+    public void showFive() {
+        System.out.println("Можно взять от 1 до 3 спичек !!!");
+    }
+
 }
