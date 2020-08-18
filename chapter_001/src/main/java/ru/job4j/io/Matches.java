0ff249e8a9ff0caf4cdddab20c4b13f1092a7d3e
Sergei Baranov <mariosb84@mail.ru>
2020-08-18 22:24:18 +0300

    2. Scanner и чтение числа из консоли. [#328794]

--- chapter_001/src/main/java/ru/job4j/io/Matches.java
+++ chapter_001/src/main/java/ru/job4j/io/Matches.java
@@ -8,28 +8,20 @@ public class Matches {
         Scanner input = new Scanner(System.in);
         boolean run = true;
         int amount = 11;
-        int countOne = 0;
-        int countTwo = 0;
+        int count = 0;
         while (run) {
-            System.out.println("Берите спичку(от 1 до 3)");
-            System.out.println("Игрок 1: ");
+            matches.showEight();
+            if (count % 2 == 0) {
+                matches.showSix();
+            } else {
+                matches.showSeven();
+            }
             int select = Integer.valueOf(input.nextLine());
-            Boolean validationOne = select <= 3 && select >= 1;
-            if (validationOne) {
+            boolean validation = select <= 3 && select >= 1;
+            if (validation) {
                 amount -= select;
                 matches.showFour(amount);
-                countOne++;
-            } else {
-                matches.showFive();
-                continue;
-            }
-            System.out.println("Игрок 2: ");
-            int selectTwo = Integer.valueOf(input.nextLine());
-            Boolean validationTwo = selectTwo <= 3 && selectTwo >= 1;
-            if (validationTwo) {
-                amount -= selectTwo;
-                matches.showFour(amount);
-                countTwo++;
+                count++;
             } else {
                 matches.showFive();
                 continue;
@@ -37,17 +29,14 @@ public class Matches {
             if (amount < 1) {
                 run = false;
                 matches.showThree();
-                if (countOne > countTwo) {
-                    matches.showOne();
-                } else {
+                if (count % 2 == 0) {
                     matches.showTwo();
+                } else {
+                    matches.showOne();
                 }
-                }
-        }
             }
-
-
-
+        }
+    }
     public void showOne() {
         System.out.println("Выиграл 1 игрок");
     }
@@ -62,6 +51,15 @@ public class Matches {
     }
     public void showFive() {
         System.out.println("Можно взять от 1 до 3 спичек !!!");
+    }
+    public void showSix() {
+        System.out.println("Игрок 1: ");
+    }
+    public void showSeven() {
+        System.out.println("Игрок 2: ");
+    }
+    public void showEight() {
+        System.out.println("Возьмите  спичку(от 1 до 3)");
     }
 
 }
