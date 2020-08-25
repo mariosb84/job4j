0120e1e31de2ee1139bf8ff27e630d0086089bb1
Sergei Baranov <mariosb84@mail.ru>
2020-08-25 10:09:00 +0300

    3. Интерфейс Input [#328795]

--- chapter_001/src/main/java/ru/job4j/poly/Bus.java
+++ chapter_001/src/main/java/ru/job4j/poly/Bus.java
@@ -1,4 +1,21 @@
 package ru.job4j.poly;
 
-public class Bus  {
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Start of motion");
+    }
+
+    @Override
+    public void passengers(int num) {
+
+    }
+
+    @Override
+    public int fill(int fuel) {
+        int price = 48;
+        return price * fuel;
+
+    }
+
 }
