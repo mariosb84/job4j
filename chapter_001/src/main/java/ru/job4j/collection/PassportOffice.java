6c5ed872123194c9e333020771592de8ba4db5fb
Sergei Baranov <mariosb84@mail.ru>
2020-10-24 20:04:47 +0300

    1. Паспорт и жители [#328781]

--- chapter_001/src/main/java/ru/job4j/collection/PassportOffice.java
+++ chapter_001/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,36 +8,21 @@ public class PassportOffice {
 
     public boolean add(Citizen citizen) {
         boolean rsl = false;
-        for (String key : citizens.keySet()) {
-        if (citizens.containsKey(key)) {
-        citizens.put(citizen.getPassport(), citizen);
-        rsl = true;
-        break;
-    }
-}
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
         return rsl;
     }
 
 
     public Citizen get(String passport) {
         for (String key : citizens.keySet()) {
-           Citizen citizen = citizens.get(key);
-            if (citizen.getPassport().equals(passport)) {
-                return new Citizen(citizen.getPassport(), citizen.getUsername());
-            }
+            Citizen value = citizens.get(key);
+           if (value.getPassport().equals(passport)) {
+               return value;
+           }
         }
         return null;
-    }
-    // Map.containsKey(key) и Map.get(key)
-
-    public static void main(String[] args) {
-        Map<String, Citizen> citizens2 = new HashMap<>();
-        PassportOffice passportOffice = new PassportOffice();
-        Citizen citizen = new Citizen("12345", "abc");
-        citizens2.put(citizen.getPassport(), citizen);
-       System.out.println(passportOffice.add(citizen));
-        System.out.println(passportOffice.get(citizen.getPassport()));
-        System.out.println(citizen.getPassport());
-        System.out.println(citizen.getUsername());
     }
 }
