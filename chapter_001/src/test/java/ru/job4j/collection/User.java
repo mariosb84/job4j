6c0b7d07e4548e053e662ae4d2c19f101b09b22e
Sergei Baranov <mariosb84@mail.ru>
2020-11-05 17:46:41 +0300

    2. Комбинированный компаратор [#328769]

---
+++ chapter_001/src/test/java/ru/job4j/collection/User.java
@@ -0,0 +1,58 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        Set<User> users = new TreeSet<>();
+        if (this.name.equals(o.getName())) {
+           // users.g
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age
+                && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
