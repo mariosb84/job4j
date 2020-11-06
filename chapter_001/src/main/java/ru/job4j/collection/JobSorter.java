86e305670a27efe483f04c3cce698e40e5eae31d
Sergei Baranov <mariosb84@mail.ru>
2020-11-06 17:38:16 +0300

    2. Комбинированный компаратор [#328769]

--- chapter_001/src/main/java/ru/job4j/collection/JobSorter.java
+++ chapter_001/src/main/java/ru/job4j/collection/JobSorter.java
@@ -8,12 +8,12 @@ import java.util.List;
 public class JobSorter {
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bug", 1),
-                new Job("Fix bug", 4),
-                new Job("Fix bug", 2),
-                new Job("X task", 0)
+               // new Job("Fix bug", 1),
+               // new Job("Fix bug", 4),
+                new Job("d", 2),
+                new Job("f", 0)
         );
-        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+       /* Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
         System.out.println();
         Comparator<Job> comb = new JobDescByName()
@@ -21,5 +21,14 @@ public class JobSorter {
                 .thenComparing(new JobDescByNameLn());
         Collections.sort(jobs, comb);
         System.out.println(jobs);
+        System.out.println();*/
+        Collections.sort(jobs, new JobDescByPriorityUp().thenComparing(new JobDescByNameUp()));
+        Comparator<Job> cmpNamePriority2 = new JobDescByPriorityUp().thenComparing(new JobDescByNameUp());
+        int rsl = cmpNamePriority2.compare(
+                new Job("f", 2),
+                new Job("a", 2)
+        );
+        System.out.println(jobs);
+        System.out.println(rsl);
     }
 }
--- chapter_001/src/test/java/ru/job4j/collection/JobTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/JobTest.java
@@ -1,18 +1,123 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
-import static org.hamcrest.Matchers.lessThan;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
     @Test
-    public void whenCompatorByNameAndPrority() {
+    public void whenJobDescByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("d", 1),
+                new Job("e", 4),
+                new Job("f", 2)
+
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("f", 1),
+                new Job("e", 4),
+                new Job("d", 2)
+        );
+        Collections.sort(jobs, new JobDescByName());
+        assertThat(jobs.get(0).getName(), is(expect.get(0).getName()));
+        assertThat(jobs.get(1).getName(), is(expect.get(1).getName()));
+        assertThat(jobs.get(2).getName(), is(expect.get(2).getName()));
+    }
+    @Test
+    public void whenJobDescByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("d", 1),
+                new Job("e", 4),
+                new Job("f", 2)
+
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("e", 4),
+                new Job("f", 2),
+                new Job("d", 1)
+        );
+        Collections.sort(jobs, new JobDescByPriority());
+        assertThat(jobs.get(0).getName(), is(expect.get(0).getName()));
+        assertThat(jobs.get(1).getName(), is(expect.get(1).getName()));
+        assertThat(jobs.get(2).getName(), is(expect.get(2).getName()));
+    }
+    @Test
+    public void whenJobDescByNameUp() {
+        List<Job> jobs = Arrays.asList(
+                new Job("e", 1),
+                new Job("d", 4),
+                new Job("f", 2)
+
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("d", 1),
+                new Job("e", 4),
+                new Job("f", 2)
+        );
+        Collections.sort(jobs, new JobDescByNameUp());
+        assertThat(jobs.get(0).getName(), is(expect.get(0).getName()));
+        assertThat(jobs.get(1).getName(), is(expect.get(1).getName()));
+        assertThat(jobs.get(2).getName(), is(expect.get(2).getName()));
+    }
+    @Test
+    public void whenJobDescByPriorityUp() {
+        List<Job> jobs = Arrays.asList(
+                new Job("d", 1),
+                new Job("e", 4),
+                new Job("f", 2)
+
+        );
+        List<Job> expect = Arrays.asList(
+                new Job("d", 1),
+                new Job("f", 2),
+                new Job("e", 4)
+        );
+        Collections.sort(jobs, new JobDescByPriorityUp());
+        assertThat(jobs.get(0).getName(), is(expect.get(0).getName()));
+        assertThat(jobs.get(1).getName(), is(expect.get(1).getName()));
+        assertThat(jobs.get(2).getName(), is(expect.get(2).getName()));
+    }
+    @Test
+    public void whenCompatorByNameAndPriority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("f", 0),
+                new Job("d", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, is(-2));
+    }
+    @Test
+    public void whenCompatorByPriorityAndName() {
+        Comparator<Job> cmpNamePriority = new JobDescByPriority().thenComparing(new JobDescByName());
+        int rsl = cmpNamePriority.compare(
+                new Job("f", 3),
+                new Job("d", 1)
+        );
+        assertThat(rsl, is(-1));
+    }
+    @Test
+    public void whenCompatorByPriorityUpAndNameUp() {
+        Comparator<Job> cmpNamePriority = new JobDescByPriorityUp().thenComparing(new JobDescByNameUp());
+        int rsl = cmpNamePriority.compare(
+                new Job("f", 4),
+                new Job("d", 2)
+        );
+        assertThat(rsl, is(1));
+    }
+    @Test
+    public void whenCompatorByNameUpAndPriorityUp() {
+        Comparator<Job> cmpNamePriority = new JobDescByNameUp().thenComparing(new JobDescByPriorityUp());
+        int rsl = cmpNamePriority.compare(
+                new Job("f", 4),
+                new Job("a", 2)
+        );
+        assertThat(rsl, is(5));
     }
 }
+
