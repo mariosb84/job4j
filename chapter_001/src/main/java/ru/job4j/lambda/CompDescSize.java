6af6bced4ec01522563c5d5ca86ec4dd5ef0a53e
Sergei Baranov <mariosb84@mail.ru>
2020-12-11 10:25:21 +0300

    2.2. Лямбда блок [#328612]

--- chapter_001/src/main/java/ru/job4j/lambda/CompDescSize.java
+++ chapter_001/src/main/java/ru/job4j/lambda/CompDescSize.java
@@ -1,11 +1,29 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class CompDescSize {
-    Comparator<String> cmpDescSize = (left, right) -> {
-        System.out.println("compare - " + Math.max(left.length(), right.length()) + " : " + Math.min(left.length(), right.length()));
-        return Integer.compare(Math.max(left.length(), right.length()), Math.min(left.length(), right.length()));
-    };
+    public static void main(String[] args) {
+        /*Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + Math.max(left.length(), right.length()) + " : " + Math.min(left.length(), right.length()));
+            return Integer.compare(Math.min(left.length(), right.length()), Math.max(left.length(), right.length()));
+        };*/
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+        List<String> strings = Arrays.asList(
+                "image 001",
+                "image 0011",
+                "image 00111"
+        );
+        strings.sort(cmpDescSize);
+        System.out.println(strings);
+    }
+
+
 }
+
 
