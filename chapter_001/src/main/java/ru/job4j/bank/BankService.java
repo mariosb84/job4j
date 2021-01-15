c3243d86639c512290190b91e3018a9ef4a695cd
Sergei Baranov <mariosb84@mail.ru>
2021-01-16 00:32:32 +0300

    3. Optional в банковских переводах. [#328633]

--- chapter_001/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_001/src/main/java/ru/job4j/bank/BankService.java
@@ -10,31 +10,35 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-       // User user = findByPassport(passport);                           // Переделываем на Optional
+        //User user = findByPassport(passport);                            // Переделываем на Optional
         Optional<User> user = findByPassport(passport);
        // if (user != null) {                                              // Переделываем на Optional
-        if (user.isPresent()) {
-            List<Account> userAccount = users.get(user);
+        if (user.isPresent()) {                                            // Переделываем на Optional
+            List<Account> userAccount = users.get(user.get());             // Переделываем на Optional
+           // List<Account> userAccount = users.get(user);                 // Переделываем на Optional
             if (!userAccount.contains(account)) {
                 userAccount.add(account);
-               // users.put(user, userAccount);
-                users.put(user, userAccount);
+               // users.put(user, userAccount);                            // Переделываем на Optional
+                users.put(user.get(), userAccount);                        // Переделываем на Optional
             }
         }
     }
 
-    //public User findByPassport(String passport) {                        // Переделываем на Optional
+    //public User findByPassport(String passport) {                       // Переделываем на Optional
     public Optional<User> findByPassport(String passport) {
         Optional<User> rsl = Optional.empty();                            // Переделываем на Optional
         for (User user : users.keySet()) {
              if (user.getPassport().equals(passport)) {
-                 rsl = Optional.of(user);
+                 rsl = Optional.of(user);                                 // Переделываем на Optional
                 // return user;                                           // Переделываем на Optional
                  break;                                                   // Переделываем на Optional
              }
         }
         //return null;                                                    // Переделываем на Optional
         return rsl;                                                       // Переделываем на Optional
+
+
+
 
        /* return users.keySet()                                                 // Переделываем на stream api :
                 .stream()
@@ -43,18 +47,28 @@ public class BankService {
                 .orElse(null);*/
     }
 
-    public Account findByRequisite(String passport, String requisite) {
-       /*     User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> accounts = users.get(user);
+    //public Account findByRequisite(String passport, String requisite) {            // Переделываем на Optional
+    public Optional<Account> findByRequisite(String passport, String requisite) {    // Переделываем на Optional
+            //User user = findByPassport(passport);                                  // Переделываем на Optional
+        Optional<User> user = findByPassport(passport);                              // Переделываем на Optional
+        Optional<Account> rsl = Optional.empty();                                    // Переделываем на Optional
+        //if (user != null) {                                                        // Переделываем на Optional
+        if (user.isPresent()) {                                                      // Переделываем на Optional
+           // List<Account> accounts = users.get(user);                              // Переделываем на Optional
+            List<Account> accounts = users.get(user.get());                          // Переделываем на Optional
             for (Account account : accounts) {
                 if (account.getRequisite().equals(requisite)) {
-                    return account;
+                   // return account;                                                // Переделываем на Optional
+                    rsl = Optional.of(account);                                      // Переделываем на Optional
+                    break;                                                           // Переделываем на Optional
                 }
             }
         }
-        return null;*/                                                          // Переделываем на stream api :
-        User user = findByPassport(passport);
+        //return null;                                                               // Переделываем на Optional
+        return rsl;                                                                  // Переделываем на Optional
+
+
+       /* User user = findByPassport(passport);                                   // Переделываем на stream api :
         if (user != null) {
             return users.get(user)
                     .stream()
@@ -62,18 +76,24 @@ public class BankService {
                     .findFirst()
                     .orElse(null);
         }
-        return null;
+        return null;*/
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account out = findByRequisite(srcPassport, srcRequisite);
-        Account in = findByRequisite(destPassport, destRequisite);
-        if (out != null && in != null && out.getBalance() >= amount) {
-           in.setBalance(in.getBalance() + amount);
-           out.setBalance(out.getBalance() - amount);
-           rsl = true;
+       // Account out = findByRequisite(srcPassport, srcRequisite);                       // Переделываем на Optional
+       // Account in = findByRequisite(destPassport, destRequisite);                      // Переделываем на Optional
+        Optional<Account> out = findByRequisite(srcPassport, srcRequisite);               // Переделываем на Optional
+        Optional<Account> in = findByRequisite(destPassport, destRequisite);              // Переделываем на Optional
+        //if (out != null && in != null && out.getBalance() >= amount) {                  // Переделываем на Optional
+            //if (out.getBalance() >= amount) {                                           // Переделываем на Optional
+           //in.setBalance(in.getBalance() + amount);                                     // Переделываем на Optional
+          // out.setBalance(out.getBalance() - amount);                                   // Переделываем на Optional
+        if (out.isPresent() && in.isPresent() && out.get().getBalance() >= amount) {                                                 // Переделываем на Optional
+            in.get().setBalance(in.get().getBalance() + amount);                                      // Переделываем на Optional
+            out.get().setBalance(out.get().getBalance() - amount);                                    // Переделываем на Optional
+            rsl = true;
         }
         return rsl;
     }
--- chapter_001/src/test/java/ru/job4j/bank/BankServiceTest.java
+++ chapter_001/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -2,6 +2,8 @@ package ru.job4j.bank;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,7 +14,8 @@ public class BankServiceTest {
         User user = new User("3434", "Petr Arsentev");
         BankService bank = new BankService();
         bank.addUser(user);
-        assertThat(bank.findByPassport("3434"), is(user));
+        //assertThat(bank.findByPassport("3434"), is(user));                                                            // Переделываем на Optional
+        assertThat(bank.findByPassport("3434").get(), is(user));                                                        // Переделываем на Optional
     }
 
     @Test
@@ -21,7 +24,8 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        //assertNull(bank.findByRequisite("34", "5546"));                                                               // Переделываем на Optional
+        assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));                             // Переделываем на Optional
     }
 
     @Test
@@ -30,7 +34,8 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+        //assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));                                       // Переделываем на Optional
+        assertThat(bank.findByRequisite("3434", "5546").get().getBalance(), is(150D));              // Переделываем на Optional
     }
 
     @Test
@@ -41,6 +46,7 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+        //assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));                           // Переделываем на Optional
+        assertThat(bank.findByRequisite(user.getPassport(), "113").get().getBalance(), is(200D));                // Переделываем на Optional
     }
 }
